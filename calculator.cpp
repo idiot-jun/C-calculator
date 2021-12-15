64b1008a3d13e06ae66f5461038f547675b5743a
yangjiajun <17607051257@163.com>
2021-12-15 21:43:34 +0800

    fix calculator

--- calculator.cpp
+++ calculator.cpp
@@ -1,184 +1,165 @@
-#include<stdio.h>
-#include<iostream>
-#include<string>
+#include"iostream"
+#include "stack"
+#include <cctype>
+#include "math.h"
 using namespace std;
-int Test(string my_print){
-    auto len=my_print.size();
-    int i=0;
-    //记录括号输入正不正确
-    int k=0;
-    //while循环验证输入的值
-while(i<len){
-        if(!((my_print[i] >= '0' && my_print[i] <= '9')
-             || my_print[i] == '+' || my_print[i] == '-' || my_print[i] == '*'
-             || my_print[i] == '/'
-             || my_print[i] == '.' || my_print[i] == '(' || my_print[i] == ')'))
+//判断优先级
+bool priority_one(char one_char) {
+    if(one_char=='+'||one_char=='-'){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool priority_two(char two_char)
+{
+    if(two_char=='*'||two_char=='/'){
+        return true;
+    }else{
+        return false;
+    }
+}
+//修改为后缀表达式
+void calculator_postfix(string &my_input) {
+    stack<char>stack;
+    string result;
+    for (int i = 0; i < my_input.size(); i++)
+    {
+        if (isdigit(my_input[i]) || my_input[i] == '.')
         {
-            cout<<"请输入0-9并且计算方式是+-*/！"<<endl;
-            return false;
-        }
-        if(my_print[i] == '(')
-            k++;
-        else if(my_print[i] == ')'){
-            k--;
-            if(k<0)return 0;
-        }
-        i++;
-}
-if(k!=0){
-        cout<<"括号输入错误！"<<endl;
-        return 0;
-}
-    return 1;
-}
-string stringtoarr(const string str){
-    int i=0,z=0,s,temp;
-    float rate=10.0,itemp=0.0;
-    float valueStr=0.0;
-    float a[40];
-    char b[20];
-    for(i=0;i<40;i++)
-        a[i]=0.0;                       
-    for(i=0;i<20;i++)
-        b[i]=' ';
-        i=0;
-    for(i=0;i<str[i];i++){
-        if(str[i]>='0'&& str[i]<='9')
-        {
-            if(rate==10.0)
-                itemp=itemp*rate+(str[i]-'0');
-            else
+            while (isdigit(my_input[i]) || my_input[i] == '.')
             {
-                itemp=itemp+rate*(str[i]-'0');
-                rate=rate/10;
+                //这里使用i++主要是为了后面计算两位数以上的数字。
+                result += my_input[i++];
+            }
+            //通过i--让i的值回到以前的值。
+            i--;
+            result += ' ';
+        }
+        else if (priority_one(my_input[i])) {
+            while (stack.size() && (priority_one(stack.top()) || priority_two(stack.top()))) {
+                result += stack.top();
+                stack.pop();
+            }
+            stack.push(my_input[i]);
+        }
+        else if (my_input[i] == ')') {
+            while (stack.top() != '(') {
+                result += stack.top();
+                stack.pop();
+            }
+            stack.pop();
+        }
+        else if (priority_two(my_input[i])) {
+            //读到二级运算符
+            while (stack.size() && priority_two(stack.top()))
+            {
+                result += stack.top();
+                stack.pop();
+            }
+            stack.push(my_input[i]);//压入二级运算符
+        }
+        else
+            stack.push(my_input[i]);
+    }
+    while (stack.size())
+    {
+        result += stack.top();
+        stack.pop();
+    }
+    my_input=result;
+}
+//实现了大于9以及小数的计算
+double double_digit(string s) {
+    double result=0;
+    double point_num=0;
+    int index=0;
+    int length=s.size();
+    string left_stu;
+    string right_stu;
+    for(int i=0;i<length;i++){
+        if(s[i]!='.'){
+            result=result*10+s[i]-'0';
+        }else{
+            index=i;
+            left_stu=s.substr(0,index-1);
+            right_stu=s.substr(index+1,length-i-i);
+            for(auto i:left_stu){
+                result=result*10+i-'0';
+            }
+            for(auto i:right_stu){
+                point_num=point_num*10+i-'0';
+            }
+            double num=point_num/pow(10,right_stu.size());
+            result=result+num;
+            return result;
+        }
+    }
+    return result;
+}
+double Calculate(string my_input) {
+    double res ,t;
+    //数字的栈
+    stack<double>val_num;
+    string temp;
+    for (int i = 0; i < my_input.size(); i++) {
+        temp = "";
+        if (isdigit(my_input[i]) || my_input[i] == '.') {
+            while (isdigit(my_input[i]) || my_input[i] == '.') {
+                temp += my_input[i++];
+            }
+            val_num.push(double_digit(temp));
+        }
+        else {
+            switch (my_input[i])
+            {
+                case '+':
+                    t = val_num.top();
+                    val_num.pop();
+                    t += val_num.top();
+                    val_num.pop();
+                    val_num.push(t);
+                    break;
+                case '-':
+                    t = val_num.top();
+                    val_num.pop();
+                    t= val_num.top() - t;
+                    val_num.pop();
+                    val_num.push(t);
+                    break;
+                case '*':
+                    t = val_num.top();
+                    val_num.pop();
+                    t *= val_num.top();
+                    val_num.pop();
+                    val_num.push(t);
+                    break;
+                case '/':
+                    t = val_num.top();
+                    val_num.pop();
+                    t= val_num.top() / t;
+                    val_num.pop();
+                    val_num.push(t);
+                    break;
+                default:
+                    cerr << "运算错误" << endl;
+                    break;
             }
         }
-        else if(str[i]=='.')    rate=0.1;
-        else if(str[i]=='+'){
-            a[z]=itemp;itemp=0;
-            z++;
-            b[z]='+';
-            z++;
-            rate=10.0;
-        }
-        else if(str[i]=='-')
-        {
-            a[z]=itemp;itemp=0;
-            z++;
-            b[z]='-';
-            z++;
-            rate=10.0;
-        }
-        else if(str[i]=='*')
-        {
-            a[z]=itemp;itemp=0;
-            b[z]='*';
-            z++;
-            rate=10.0;
-        }
-        else if(str[i]=='/')
-        {
-            a[z]=itemp;itemp=0;
-            z++;
-            b[z]='/';
-            z++;
-            rate=10.0;
-        }
     }
-    a[z]=itemp;
-    s=z;
-    for(i=0;i<=s;i++)
-    {
-        if(b[i]=='/')
-        {
-            a[i+1]=1/a[i+1];
-            b[i]='*';
-        }
-    }
-    for(i=0;i<=s;i++)
-    {
-        while(b[i]=='*')
-        {
-            a[i-1]=a[i-1]*a[i+1];
-            for(temp=i+1;temp+2<40;temp++)
-                a[temp]=a[temp+2];
-            for(temp=i;temp+2<20;temp++)
-                b[temp]=b[temp+2];
-        }
-    }
-    valueStr=a[0];
-    for(i=0;i<s;i++)
-    {
-        if(b[i]=='+')
-            valueStr+=a[i+1];
-        else if(b[i]=='-')
-            valueStr-=a[i+1];
-    }
-    char svalue[16];
-    sprintf(svalue,"%f" , valueStr);
-    string ssavalue(svalue);
-    return ssavalue;
+    res = val_num.top();
+    return res;
 }
-string trims(const string &sub){
-    string s1,s2,s3,sp,ss;
-    int i;
-    int counter=0;
-    int left,right;
-    if(auto pos1=sub.find(")")==string::npos)
-        return sub;
-    for(i=0;sub[i];i++)
-        if(sub[i]=='(')
-        {
-            left=i;
-            counter++;
+int main() {
+    string my_input, result;
+    while(true) {
+        cout << "输入表达式：" << endl;
+        cin >> my_input;
+        if(my_input=="exit"){
             break;
         }
-    for(i=left+1;sub[i];i++)
-    {
-        if(sub[i]=='(')
-            counter++;
-        else if(sub[i]==')')
-            counter--;
-        if(counter==0)
-        {
-            right=i;
-            break;
-        }
+        calculator_postfix(my_input);
+        cout << "运算结果：" << Calculate(my_input) << endl;
     }
-    s1=sub.substr(0,left);
-    sp=sub.substr(left+1,right-left-1);
-    s2=stringtoarr(trims(sub.substr(left+1,right-left-1)));
-    s3=trims(sub.substr(right+1));
-    ss=s1+s2+s3;
-    return ss;
-}
-void calcation(string &str1){
-    int stTe;
-    if((stTe=Test(str1))<=0){
-        cout<<"输入字符串有误，请重新输入！！！"<<endl;
-        return;
-    }
-    string sk=trims(str1);
-    string ssk=stringtoarr(sk);
-    cout<<ssk<<endl;
-    cout<<"--------------------"<<endl;
-}
-int main()
-{
-    string input_str;
-    int tsTest;
-    while(1)
-    {
-        cout<<"请输入表达式(退出请输入小写exit):"<<endl;
-        cin>>input_str;
-        cout<<endl;
-        if(input_str.compare("exit")==0)
-            break;
-        if((tsTest=Test(input_str))==1)
-        {
-            calcation(input_str);
-        }
-    }
-    cout<<"Close this calculation！"<<endl;
     return 0;
 }
